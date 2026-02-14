9f59c5a68b727ca6385be5955d64dd8146d0d736
RadityaBagus112 <businessraditya@gmail.com>
2026-02-14 18:01:32 +0700

    use env for firebase config

--- bengkel-progress/lib/firebase.ts
+++ bengkel-progress/lib/firebase.ts
@@ -1,18 +1,17 @@
-import { initializeApp, getApps } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCVkd1084xiAQxwK6ZqNYtBpTlUeJKorMo',
-  authDomain: 'bengkel-progress.firebaseapp.com',
-  projectId: 'bengkel-progress',
-  storageBucket: 'bengkel-progress.firebasestorage.app',
-  messagingSenderId: '55239380236',
-  appId: '1:55239380236:web:79fbaf1c478ae2e2198d29',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-const app =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
