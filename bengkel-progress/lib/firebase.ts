0b90a9890deaf3482e1b5bd8948151b93e45f92f
RadityaBagus112 <businessraditya@gmail.com>
2026-02-15 00:23:46 +0700

    fix lib firebase

--- bengkel-progress/lib/firebase.ts
+++ bengkel-progress/lib/firebase.ts
@@ -3,13 +3,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
+// VALIDASI BIAR KELIHATAN KALO ENV KOSONG
+if (!firebaseConfig.apiKey) {
+  console.error("❌ Firebase ENV belum kebaca. Cek Vercel env variables!");
+}
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
